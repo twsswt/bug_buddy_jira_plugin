3491b422aeddc6e9269fff9f8a85813bf51d00d1
Stephen Brown <1106679b@student.gla.ac.uk>
2017-10-22 21:31:00 +0100

    Implement timing of extractIssueComments

--- evaluation-harness/src/main/Main.java
+++ evaluation-harness/src/main/Main.java
@@ -9,21 +9,30 @@ class Main {
 
     public static void main(String[] args) {
 
+        int maxIssuesToDownload = Integer.MAX_VALUE;
+        if (args.length > 0) {
+            maxIssuesToDownload = Integer.parseInt(args[0]);
+        }
+
         ArrayList<FirefoxIssue> issues;
 
         // Extract data from the Bug Database CSV File
         CSVIssueReader reader = new CSVIssueReader();
         issues = reader.readIssuesFromCSV("/home/stephen/bug_buddy_jira_plugin/project-issue-data/bugreport.mozilla.firefox/mozilla_firefox_bugmeasures.csv");
-        System.out.println(issues);
+
+        if (issues.size() < maxIssuesToDownload) {
+            maxIssuesToDownload = issues.size();
+        }
 
         // Get the comments for each bug
         // Comments aren't provided in the Bug Database
         Scraper s = new Scraper();
-        for (FirefoxIssue issue: issues) {
-            ArrayList<String> comments = s.extractIssueComments(issue);
-            issue.setComments(comments);
-            System.out.println("Set issue " + issue.getBugID() + "!");
-            System.out.println(issue);
+        for (int i = 0; i < maxIssuesToDownload; i++) {
+            //s.getIssueXML(issues.get(i));
+            ArrayList<String> comments = s.extractIssueComments(issues.get(i));
+            issues.get(i).setComments(comments);
+            System.out.println("Set issue " + issues.get(i).getBugID() + "!");
+            //System.out.println(issues.get(i));
         }
 
     }
--- evaluation-harness/src/scraper/Scraper.java
+++ evaluation-harness/src/scraper/Scraper.java
@@ -7,24 +7,22 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class Scraper {
 
     /**
-     * getIssueXML will download the XML version of the specified issue
+     * getIssueXML will download the XML version of the specified issue, and save
+     * to a file called bugID.xml
      * @param issue The issue which we want to get an XML version of
      * @return A string containing the XML of the specified issue
      */
     public String getIssueXML(FirefoxIssue issue) {
         String issueURL = "https://bugzilla.mozilla.org/show_bug.cgi?ctype=xml&id=" + issue.getBugID();
 
-        StringBuilder xmlDocument = new StringBuilder();
+        StringBuilder xmlDocumentBuffer = new StringBuilder();
 
         try {
 
@@ -35,15 +33,28 @@ public class Scraper {
 
             String line;
             while ((line = br.readLine()) != null) {
-                xmlDocument.append(line);
-                xmlDocument.append("\n");
+                xmlDocumentBuffer.append(line);
+                xmlDocumentBuffer.append("\n");
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return xmlDocument.toString();
+        String xmlDocument = xmlDocumentBuffer.toString();
+
+        String outputFilename = "/home/stephen/bug_buddy_jira_plugin/project-issue-data/bugreport.mozilla.firefox/issueXML/" + issue.getBugID() + ".xml";
+
+        File output = new File(outputFilename);
+        if (!output.exists()) {
+            try {
+                PrintWriter out = new PrintWriter(outputFilename);
+                out.print(xmlDocumentBuffer);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return xmlDocument;
     }
 
     /**
@@ -53,13 +64,20 @@ public class Scraper {
      * @return an ArrayList containing each comment on the issue
      */
     public ArrayList<String> extractIssueComments(FirefoxIssue issue) {
+        long downloadTime = 0;
+
+        long functionTimeStart = System.currentTimeMillis();
         ArrayList<String> comments = new ArrayList<>();
         String issueURL = "https://bugzilla.mozilla.org/show_bug.cgi?ctype=xml&id=" + issue.getBugID();
 
         try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            long downloadTimeStart = System.currentTimeMillis();
             Document doc = dBuilder.parse(issueURL);
+            long downloadTimeFinish = System.currentTimeMillis();
+
+            downloadTime = downloadTimeFinish - downloadTimeStart;
 
             // Get rid of extra line breaks in downloaded XML
             doc.getDocumentElement().normalize();
@@ -74,6 +92,15 @@ public class Scraper {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        long functionTimeEnd = System.currentTimeMillis();
+
+        long functionTime = functionTimeEnd - functionTimeStart;
+
+        double finalPercent = (downloadTime*100.0f)/functionTime;
+
+        System.out.println("Function Time: " + functionTime);
+        System.out.println("Download Time: " + downloadTime);
+        System.out.printf("Percent: %.2f%%\n" ,finalPercent );
         return comments;
     }
 
