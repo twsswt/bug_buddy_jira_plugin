a2e787e21e6004a4d4ab4b0f2e089c8babe6b7d8
Stephen Brown <1106679b@student.gla.ac.uk>
2017-10-31 20:48:17 +0000

    Implement JiraProject class

---
+++ evaluation-harness/src/converter/Converter.java
@@ -0,0 +1,15 @@
+package converter;
+
+import main.FirefoxIssue;
+
+public class Converter {
+    public String projectKey;
+
+    public Converter() {
+
+    }
+    public void convertIssueToJiraJSON(FirefoxIssue issue) {
+
+
+    }
+}
---
+++ evaluation-harness/src/main/JiraProject.java
@@ -0,0 +1,117 @@
+package main;
+
+public class JiraProject {
+    private String key;
+    private String name;
+    private String projectTypeKey;
+    private String projectTemplateKey;
+    private String description;
+    private String lead;
+    private String url;
+    private String assigneeType;
+    private int avatarID;
+    private int permissionScheme;
+    private int notificationScheme;
+
+    public JiraProject() {
+        key = "FRFX";
+        name = "Firefox Issues";
+        projectTypeKey = "business";
+        projectTemplateKey = "com.atlassian.jira-core-project-templates:jira-core-project-management";
+        description = "Firefox data set for recommendations";
+        lead = "admin";
+        url = "https://atlassian.com";
+        assigneeType = "PROJECT_LEAD";
+        avatarID = 10200;
+        permissionScheme = 0;
+        notificationScheme = 10000;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProjectTypeKey() {
+        return projectTypeKey;
+    }
+
+    public void setProjectTypeKey(String projectTypeKey) {
+        this.projectTypeKey = projectTypeKey;
+    }
+
+    public String getProjectTemplateKey() {
+        return projectTemplateKey;
+    }
+
+    public void setProjectTemplateKey(String projectTemplateKey) {
+        this.projectTemplateKey = projectTemplateKey;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLead() {
+        return lead;
+    }
+
+    public void setLead(String lead) {
+        this.lead = lead;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAssigneeType() {
+        return assigneeType;
+    }
+
+    public void setAssigneeType(String assigneeType) {
+        this.assigneeType = assigneeType;
+    }
+
+    public int getAvatarID() {
+        return avatarID;
+    }
+
+    public void setAvatarID(int avatarID) {
+        this.avatarID = avatarID;
+    }
+
+    public int getPermissionScheme() {
+        return permissionScheme;
+    }
+
+    public void setPermissionScheme(int permissionScheme) {
+        this.permissionScheme = permissionScheme;
+    }
+
+    public int getNotificationScheme() {
+        return notificationScheme;
+    }
+
+    public void setNotificationScheme(int notificationScheme) {
+        this.notificationScheme = notificationScheme;
+    }
+}
