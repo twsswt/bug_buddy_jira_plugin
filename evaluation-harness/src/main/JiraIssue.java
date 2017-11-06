0ee854ba671549ca1bd31194c7f0a7ad8706df89
Stephen Brown <1106679b@student.gla.ac.uk>
2017-11-06 18:45:02 +0000

    Add fields of Jira Issues

---
+++ evaluation-harness/src/main/JiraIssue.java
@@ -0,0 +1,13 @@
+package main;
+
+public class JiraIssue {
+    private String projectKey;
+    private String summary;
+    private String description;
+    private String issueTypeID;
+
+    // Need to create these users in Jira, if they don't already exist
+    private String reporterName;
+    private String assigneeName;
+
+}
