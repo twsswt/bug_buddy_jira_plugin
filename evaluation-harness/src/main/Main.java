8db04b6356a016452a61199c1493051e598341f9
Stephen Brown <1106679b@student.gla.ac.uk>
2017-11-19 17:26:45 +0000

    Push items to JIRA

--- evaluation-harness/src/main/Main.java
+++ evaluation-harness/src/main/Main.java
@@ -4,10 +4,7 @@ import converter.Converter;
 import scraper.CSVIssueReader;
 import scraper.Scraper;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -24,12 +21,14 @@ class Main {
         ArrayList<FirefoxIssue> firefoxIssues = getIssueData();
         ArrayList<JiraIssue> jiraIssues = new ArrayList<>();
 
+        String issueJSONlocation = "../project-issue-data/bugreport.mozilla.firefox/issueJSON/";
+
         Converter converter = new Converter();
 
         // Create a JIRA Project json, and write to file
         JiraProject jiraProject = new JiraProject();
         String jiraProjectJson = converter.convertJiraProjectToJiraJSON(jiraProject);
-        String jiraProjectJsonFilename = "../project-issue-data/bugreport.mozilla.firefox/issueJSON/project.json";
+        String jiraProjectJsonFilename = issueJSONlocation + "project.json";
 
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(jiraProjectJsonFilename));
@@ -50,7 +49,7 @@ class Main {
         // Create Jira users from each email address, and write to a file
         for (String email: userEmails) {
             String userJson = converter.convertEmailAddressToJiraUser(email);
-            String userJsonFilename = "../project-issue-data/bugreport.mozilla.firefox/issueJSON/users/" + email + ".json";
+            String userJsonFilename = issueJSONlocation + "users/" + email + ".json";
 
             try {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(userJsonFilename));
@@ -60,6 +59,81 @@ class Main {
                 e.printStackTrace();
             }
         }
+
+        // Create jira json from every issue, and write to a file
+        for (FirefoxIssue firefoxIssue: firefoxIssues) {
+            JiraIssue jiraIssue = converter.convertFirefoxIssueToJiraIssue(firefoxIssue);
+            String issueJson = converter.convertJiraIssueToJiraJSON(jiraIssue);
+            String issueJsonFilename = issueJSONlocation + "issues/" + firefoxIssue.getBugID() + ".json";
+
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(issueJsonFilename));
+                writer.write(issueJson);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // TODO convert each issues comments to Jira Json
+
+        // Post the project, then all users, then all issues to Jira
+        String basicCurlCommand1 = "curl -D- -u admin:admin -X POST --data @" + issueJSONlocation;
+        String basicCurlCommand2 = " -H Content-Type:application/json http://localhost:2990/jira/rest/api/2/";
+        try {
+            String fullCurlCommand = basicCurlCommand1 + "project.json" + basicCurlCommand2 + "project";
+            System.out.println(fullCurlCommand);
+            Process p = Runtime.getRuntime().exec(fullCurlCommand);
+            p.waitFor();
+
+            InputStream stdout = p.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (String email: userEmails) {
+                String fullCurlCommand = basicCurlCommand1 + "users/" + email + ".json" + basicCurlCommand2 + "user";
+                System.out.println(fullCurlCommand);
+                Process p = Runtime.getRuntime().exec(fullCurlCommand);
+                p.waitFor();
+
+                InputStream stdout = p.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (FirefoxIssue firefoxIssue: firefoxIssues) {
+                String fullCurlCommand = basicCurlCommand1 + "issues/" + firefoxIssue.getBugID() + ".json" + basicCurlCommand2 + "issue";
+                System.out.println(fullCurlCommand);
+                Process p = Runtime.getRuntime().exec(fullCurlCommand);
+                p.waitFor();
+
+                InputStream stdout = p.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // TODO post all issue comments to JIRA
     }
 
     private static void processArguments(String[] args) {
