acbb4c355223cb2f9a128e674378b62bdd3329ee
Stephen Brown <1106679b@student.gla.ac.uk>
2017-12-28 18:01:46 +0000

    Implement pulling first 50 issues

--- recommender/src/puller/Puller.java
+++ recommender/src/puller/Puller.java
@@ -1,5 +1,9 @@
 package puller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Puller {
@@ -19,6 +23,38 @@ public class Puller {
 
     public ArrayList<JiraIssue> getAllIssues() {
         ArrayList<JiraIssue> issues = new ArrayList<>();
+
+        // Get all issues REST URL
+        //
+        // http://localhost:2990/jira/rest/api/2/search?jql=project%3D%22FRFX%22&startAt=49&maxResults=50
+        //
+        // This is returned when there are no issues left
+        // {
+        //   "startAt": 0,
+        //   "maxResults": 50,
+        //   "total": 100,
+        //   "issues": []
+        // }
+        String curlPrefix = "curl -i -u admin:admin -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X GET ";
+        String issueSearchQuery = "/search?jql=project%3D%22FRFX%22&startAt=0&maxResults=50";
+        String curlCommand = curlPrefix + jiraAPILocation + issueSearchQuery;
+
+        try {
+            Process p = Runtime.getRuntime().exec(curlCommand);
+        
+            InputStream stdout = p.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+            String line, returnedJSON = "";
+            while ((line = reader.readLine()) != null) {
+                returnedJSON += line;
+                returnedJSON += "\n";
+            }
+
+            System.out.println(returnedJSON);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return issues;
     }
 
