fa1a081014f5c14d358ebc4c0e0e864665cf2846
Stephen Brown <1106679b@student.gla.ac.uk>
2018-01-03 16:33:58 +0000

    Improve comments in classifier component

--- recommender/src/classifier/FrequencyTable.java
+++ recommender/src/classifier/FrequencyTable.java
@@ -18,14 +18,24 @@ public class FrequencyTable {
                 '}';
     }
 
+    /**
+     * Compares how similar this frequency table is with another
+     *
+     * @param other the other frequency table
+     * @return a percentage between 0.00 and 1.00, representing how similar the two frequency tables are
+     */
     public double compareSimilarity(FrequencyTable other) {
         List<Double> percentages = new ArrayList<>();
 
         for (FrequencyTableEntry entry : this.entries) {
+            // attempt to find an entry with the same word in the other table
+            // if no such entry exists then they have 0% similarity
             FrequencyTableEntry matchedEntry = other.getEntryWithWord(entry.word);
             if (matchedEntry == null) {
                 percentages.add(0.0);
             } else {
+
+                // Calculate each frequency as a percentage of their overall occurrences
                 double thisFrequencyPercentage = (entry.frequency / (double) this.entries.size()) / 100;
                 double otherFrequencyPercentage = (matchedEntry.frequency / (double) other.entries.size()) / 100;
                 double averageFrequencyPercentage = (thisFrequencyPercentage + otherFrequencyPercentage) / 2.0;
@@ -41,6 +51,9 @@ public class FrequencyTable {
         return total / (double) this.entries.size();
     }
 
+    /**
+     * Gets the entry of the frequency table with the specified word
+     */
     public FrequencyTableEntry getEntryWithWord(String word) {
         for (FrequencyTableEntry entry : entries) {
             if (entry.word.equals(word)) {
