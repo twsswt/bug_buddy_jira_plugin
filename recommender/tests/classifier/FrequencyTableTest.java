c1cd7f4107f2263ff51f95155cd69fdbd1ea7d3a
Stephen Brown <1106679b@student.gla.ac.uk>
2018-01-11 18:49:47 +0000

    Impement additional tests for Frequency Table similarity

--- recommender/tests/classifier/FrequencyTableTest.java
+++ recommender/tests/classifier/FrequencyTableTest.java
@@ -15,6 +15,47 @@ public class FrequencyTableTest {
 
         double similarity = ft.compareSimilarity(ft);
 
-        assertEquals(1, similarity, 0.01);
+        assertEquals(1, similarity, 0.0001);
+    }
+
+    @Test
+    public void testCompareSimilarityCompletelyDifferent() {
+        FrequencyTable ft1 = new FrequencyTable();
+        ft1.addEntry(new FrequencyTableEntry("The", 6));
+
+        FrequencyTable ft2 = new FrequencyTable();
+        ft2.addEntry(new FrequencyTableEntry("Cat", 7));
+
+        double similarity = ft1.compareSimilarity(ft2);
+
+        assertEquals(0, similarity, 0.0001);
+    }
+
+    @Test
+    public void testCompareSimilarityGreaterThanOther() {
+        FrequencyTableEntry cats = new FrequencyTableEntry("Cat", 7);
+        FrequencyTableEntry dogs = new FrequencyTableEntry("Dog", 6);
+        FrequencyTableEntry fish = new FrequencyTableEntry("Fish", 5);
+        FrequencyTableEntry pigs = new FrequencyTableEntry("Pigs", 4);
+
+        FrequencyTable ft1 = new FrequencyTable();
+        ft1.addEntry(cats);
+        ft1.addEntry(dogs);
+        ft1.addEntry(pigs);
+        ft1.addEntry(fish);
+
+        FrequencyTable ft2 = new FrequencyTable();
+        ft2.addEntry(cats);
+        ft2.addEntry(dogs);
+        ft2.addEntry(fish);
+
+        FrequencyTable ft3 = new FrequencyTable();
+        ft3.addEntry(cats);
+        ft3.addEntry(dogs);
+
+        double similarity12 = ft1.compareSimilarity(ft2);
+        double similarity13 = ft1.compareSimilarity(ft3);
+
+        assertTrue(similarity12 > similarity13);
     }
 }
