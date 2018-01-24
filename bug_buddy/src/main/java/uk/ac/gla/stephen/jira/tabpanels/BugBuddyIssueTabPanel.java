a0105860606a5fea4954291ce2693af3df7470e4
Stephen Brown <1106679b@student.gla.ac.uk>
2018-01-24 12:08:17 +0000

    Hardcode assignments to Tim

--- bug_buddy/src/main/java/uk/ac/gla/stephen/jira/tabpanels/BugBuddyIssueTabPanel.java
+++ bug_buddy/src/main/java/uk/ac/gla/stephen/jira/tabpanels/BugBuddyIssueTabPanel.java
@@ -1,5 +1,7 @@
 package uk.ac.gla.stephen.jira.tabpanels;
 
+import com.atlassian.jira.plugin.issuetabpanel.IssueAction;
+import com.atlassian.jira.util.EasyList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel;
@@ -7,6 +9,8 @@ import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
 import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.user.ApplicationUser;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,7 +19,10 @@ public class BugBuddyIssueTabPanel extends AbstractIssueTabPanel implements Issu
     private static final Logger log = LoggerFactory.getLogger(BugBuddyIssueTabPanel.class);
 
     public List getActions(Issue issue, ApplicationUser remoteUser) {
-        return Collections.singletonList(new GenericMessageAction("This is where the auto-assign button should be!"));
+        List<IssueAction> list = new ArrayList<>();
+        list.add(new GenericMessageAction("This is where the auto-assign button should be!"));
+        list.add(new GenericMessageAction("We recommend assigning this issue to tim.storer@gla.ac.uk"));
+        return list;
     }
 
     public boolean showPanel(Issue issue, ApplicationUser remoteUser)
