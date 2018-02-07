1d78073ca52dea19dc6de5fde56a7455e0827535
Stephen Brown <1106679b@student.gla.ac.uk>
2018-02-07 14:46:11 +0000

    Remove unused imports

--- bug_buddy/src/main/java/uk/ac/gla/stephen/jira/tabpanels/BugBuddyIssueTabPanel.java
+++ bug_buddy/src/main/java/uk/ac/gla/stephen/jira/tabpanels/BugBuddyIssueTabPanel.java
@@ -1,21 +1,18 @@
 package uk.ac.gla.stephen.jira.tabpanels;
 
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
+import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel;
 import com.atlassian.jira.plugin.issuetabpanel.IssueAction;
-import com.atlassian.jira.util.EasyList;
+import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
+import com.atlassian.jira.user.ApplicationUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel;
-import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanel;
-import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.user.ApplicationUser;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class BugBuddyIssueTabPanel extends AbstractIssueTabPanel implements IssueTabPanel
-{
+public class BugBuddyIssueTabPanel extends AbstractIssueTabPanel implements IssueTabPanel {
     private static final Logger log = LoggerFactory.getLogger(BugBuddyIssueTabPanel.class);
 
     public List getActions(Issue issue, ApplicationUser remoteUser) {
@@ -25,8 +22,7 @@ public class BugBuddyIssueTabPanel extends AbstractIssueTabPanel implements Issu
         return list;
     }
 
-    public boolean showPanel(Issue issue, ApplicationUser remoteUser)
-    {
+    public boolean showPanel(Issue issue, ApplicationUser remoteUser) {
         return true;
     }
 }
